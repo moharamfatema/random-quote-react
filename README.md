dc96725f34a4ed4ff70386130cbd708ad3ad18cc
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2022-07-27 23:44:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# random-quote
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/moharamfatema/pen/qBoPgBg](https://codepen.io/moharamfatema/pen/qBoPgBg).
+
+Generate random quotes web app react
---
+++ dist/script.js
@@ -0,0 +1,101 @@
+// !! IMPORTANT README:
+
+// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place.
+
+/***********
+INSTRUCTIONS:
+  - Select the project you would 
+    like to complete from the dropdown 
+    menu.
+  - Click the "RUN TESTS" button to
+    run the tests against the blank 
+    pen.
+  - Click the "TESTS" button to see 
+    the individual test cases. 
+    (should all be failing at first)
+  - Start coding! As you fulfill each
+    test case, you will see them go   
+    from red to green.
+  - As you start to build out your 
+    project, when tests are failing, 
+    you should get helpful errors 
+    along the way!
+    ************/
+
+// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
+function App() {
+  [quote, setQuote] = React.useState({ text: "", author: "" });
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key":
+      "9d7e8e09d3msha547eb34cc2e8a1p101babjsn557a22ba9248",
+      "X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com" } };
+
+
+  const getQuote = () => {
+    fetch(
+    "https://famous-quotes4.p.rapidapi.com/random?category=wisdom&count=1",
+    options).
+
+    then(response => response.json()).
+    then(response => {
+      console.log(response[0]);
+      setQuote({
+        text: response[0].text,
+        author: response[0].author });
+
+    }).
+    catch(err => console.error(err));
+  };
+
+  React.useEffect(getQuote, []);
+
+  return /*#__PURE__*/(
+    React.createElement("div", {
+      id: "quote-box",
+      class: "container d-flex flex-column bg-light text-center m-5 rounded-5 p-5 flex-shrink-1 " }, /*#__PURE__*/
+
+    React.createElement("div", { class: "flex-grow-1 d-flex justify-content-evenly" }, /*#__PURE__*/
+    React.createElement("p", { id: "text", class: "fs-1 " }, "\"",
+    quote.text, "\"")), /*#__PURE__*/
+
+
+    React.createElement("p", { id: "author", class: "fs-3" }, "-",
+    quote.author), /*#__PURE__*/
+
+    React.createElement("div", { class: "row g-5 justify-self-end m-3 text-center" }, /*#__PURE__*/
+    React.createElement("div", { class: "col" }, /*#__PURE__*/
+    React.createElement("button", {
+      id: "new-quote",
+      class: " btn btn-primary fs-3 flex-grow-1",
+      onClick: getQuote }, "New Quote")), /*#__PURE__*/
+
+
+
+
+    React.createElement("div", { class: "col" }, /*#__PURE__*/
+    React.createElement("a", {
+      id: "tweet-quote",
+      href: "https://twitter.com/intent/tweet",
+      class: " btn btn-outline-primary fs-3" }, /*#__PURE__*/
+
+    React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "16",
+      height: "16",
+      fill: "currentColor",
+      class: "bi bi-twitter",
+      viewBox: "0 0 16 16" }, /*#__PURE__*/
+
+    React.createElement("path", { d: "M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" })), "Tweet Quote")))));
+
+
+
+
+
+
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("root"));
